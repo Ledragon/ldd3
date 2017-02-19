94344edcaf5e99e6a7f33e5e44f7700c67d8d5ca
Hugues Stefanski <hugues.stefanski@gmail.com>
2017-02-19 15:08:43 +0100

    Moved export Charts

---
+++ src/Charts.ts
@@ -0,0 +1,5 @@
+export { HorizontalBarChart } from './charts/HorizontalBarChart';
+export { LinearLinearChart } from './charts/LinearLinearChart';
+export { CategoricalLinearChart } from './charts/CategoricalLinearChart';
+export { TimeLinearChart } from './charts/TimeLinearChart';
+export { MultiCategoricalChart } from './charts/MultiCategoricalChart';
--- src/charts/charts.ts
+++
@@ -1,5 +0,0 @@
-export { HorizontalBarChart } from './HorizontalBarChart';
-export { LinearLinearChart } from './LinearLinearChart';
-export { CategoricalLinearChart } from './CategoricalLinearChart';
-export { TimeLinearChart } from './TimeLinearChart';
-export { MultiCategoricalChart } from './MultiCategoricalChart';
