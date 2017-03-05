80cff012c67882eadb436a76eeae2f1229a76840
Hugues Stefanski <hugues.stefanski@gmail.com>
2017-03-05 14:42:18 +0100

    Rollup d3

---
+++ rollup.d3.js
@@ -0,0 +1,32 @@
+import resolve from 'rollup-plugin-node-resolve';
+
+const d3Array = [
+    'd3-array',
+    'd3-axis',
+    'd3-collection',
+    'd3-dispatch',
+    'd3-format',
+    'd3-scale',
+    'd3-selection',
+    'd3-shape',
+    'd3-time-format'
+];
+var globals = {};
+for (var i = 0; i < d3Array.length; i++) {
+    globals[d3Array[i]] = 'd3';
+}
+export default {
+    entry: 'src/d3-bundle.js',
+    dest: 'test/js/d3.js',
+    format: 'umd',
+    // globals: globals,
+    // external: d3Array,
+    plugins: [
+        resolve({
+            jsnext: true,
+            browser: true,
+            main: true
+        })
+    ],
+    moduleName: 'ldd3'
+};
